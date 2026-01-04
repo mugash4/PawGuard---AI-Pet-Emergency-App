522b36690f51e15e7215fe754576c53aa0f7db3b
mugash4 <mugash4@gmail.com>
2026-01-04 19:57:10 +0300

    chore: update dependencies to latest versions
    - Upgraded @react-navigation packages to version 7.x
    - Updated firebase to version 11.2.0
    - Bumped react and react-dom to version 19.0.0
    - Updated react-native to version 0.79.6
    - Updated react-native-paper to version 5.12.5
    - Updated @types/react to version 19.0.11
    - Adjusted expo version to 53.0.24

--- metro.config.js
+++ metro.config.js
@@ -2,4 +2,7 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+// Enable package.json exports support (required for React 19)
+config.resolver.unstable_enablePackageExports = true;
+
 module.exports = config;
